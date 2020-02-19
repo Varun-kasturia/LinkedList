651b6699dec0d01d3d5d74b4beb441d7a7a6c82f
Varun-kasturia <61134594+Varun-kasturia@users.noreply.github.com>
2020-02-19 14:58:33 +0530

    add

---
+++ LinkedList_InsertAtMiddle.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+using namespace std;
+class node
+{
+public:
+	int data;
+	node* next;
+
+	node( int d )
+	{
+		data=d;
+		next=NULL;
+	}
+};
+int Length(node*head)
+{
+	int length=0;
+	while(head!=NULL)
+	{
+		head=head->next;
+		length=length+1;
+	}
+	return length;
+}
+
+void InsertAtHead(node*&head,int data)
+{
+
+	node* n=new node(data);
+	n->next=head;
+	head=n;
+
+}
+
+void InsertAtTail(node*&head,int data)
+{
+	if(head==NULL)
+	{
+			InsertAtHead(head,data);
+			return;
+	}
+	node* tail=head;
+	while(tail->next!=NULL)
+	{
+		tail=tail->next;
+	}
+		node* n=new node(data);
+		tail->next=n;
+}
+
+void InsertAtMiddle(node*&head,int data,int p)
+{
+	if(head==NULL||p==0)
+	{
+		InsertAtHead(head,data);
+		return;
+	}
+	if(p>Length(head))
+	{
+		InsertAtTail(head,data);
+		return;
+	}
+	else
+	{
+		node* mid=head;
+	int temp=0;
+	while(temp!=p)
+	{
+		temp++;
+		mid=mid->next;
+	}
+	node* n=new node(data);
+	n->next=mid->next;
+	mid->next=n;
+	}
+}
+
+void print(node*head)
+{
+	while(head!=NULL)
+	{
+		cout<<head->data<<" -> ";
+		head=head->next;
+	}
+}
+
+int main()
+{
+	node* head=NULL;
+	InsertAtHead(head,4);
+	InsertAtTail(head,5);
+	InsertAtMiddle(head,6,0);
+	InsertAtMiddle(head,7,1);
+	print(head);
+	return 0;
+}
